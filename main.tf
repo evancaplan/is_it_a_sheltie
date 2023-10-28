c477872e04d295e1822dec476dfa418b9d483427
evancaplan <ecaplan31@gmail.com>
2023-10-28 11:41:13 -0400

    Create Sheltie API

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region = "us-east-1"
-    profile = "default"
+    profile = "evan-personal"
 }
 
 
@@ -23,7 +23,7 @@ resource "aws_iam_role" "sheltie_image_role" {
 }
 
 resource "aws_s3_bucket" "sheltie_image_comparison_bucket" {
-    bucket = "sheltie-images"
+    bucket = "sheltie-image-comparison-bucket"
 }
 
 resource "aws_s3_bucket_policy" "sheltie_image_bucket_policy" {
@@ -81,5 +81,84 @@ resource "aws_lambda_function" "sheltie_image_lambda" {
     timeout       = 10 
     memory_size   = 256  
 }
+
+resource "aws_api_gateway_api_key" "sheltie_api_key" {
+  name = "SheltieAPIKey"
+  description = "API key for Sheltie Image Recognition"
+  enabled = true
+}
+
+
+resource "aws_api_gateway_rest_api" "sheltie_api" {
+  name        = "IsItASheltieAPI"
+  description = "API for Sheltie Image Recognition"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_lambda_permission" "sheltie_api_lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sheltie_image_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.sheltie_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_resource" "sheltie_resource" {
+  rest_api_id = aws_api_gateway_rest_api.sheltie_api.id
+  parent_id   = aws_api_gateway_rest_api.sheltie_api.root_resource_id
+  path_part   = "image"
+}
+
+resource "aws_api_gateway_method" "sheltie_post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.sheltie_api.id
+  resource_id   = aws_api_gateway_resource.sheltie_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+  api_key_required = true
+}
+
+resource "aws_api_gateway_integration" "sheltie_lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.sheltie_api.id
+  resource_id = aws_api_gateway_method.sheltie_post_method.resource_id
+  http_method = aws_api_gateway_method.sheltie_post_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.sheltie_image_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "sheltie_api_deployment" {
+  depends_on  = [aws_api_gateway_integration.sheltie_lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.sheltie_api.id
+  stage_name  = "stage"
+  description = "Deploying the API for Sheltie Recognition"
+  stage_description = "Stage"
+}
+
+resource "aws_api_gateway_usage_plan" "sheltie_usage_plan" {
+  name        = "SheltieUsagePlan"
+  description = "Usage plan for Sheltie API"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.sheltie_api.id
+    stage  = aws_api_gateway_deployment.sheltie_api_deployment.stage_name
+  }
+}
+
+resource "aws_api_gateway_usage_plan_key" "sheltie_usage_plan_key" {
+  key_id        = aws_api_gateway_api_key.sheltie_api_key.id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.sheltie_usage_plan.id
+}
+
+output "api_endpoint" {
+  value = "${aws_api_gateway_deployment.sheltie_api_deployment.invoke_url}/image"
+}
+
+
+
+
 
 
